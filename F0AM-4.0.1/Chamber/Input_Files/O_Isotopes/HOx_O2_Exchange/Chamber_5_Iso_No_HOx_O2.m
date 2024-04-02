116aee653ccb69bad75b01f7fb6dab36b4460641
Walters-Research-Group <147226982+Walters-Research-Group@users.noreply.github.com>
2024-04-02 15:32:03 -0400

    without HO2/O2 Exchange
    The files now include additional simulations without considering HO2/O2 isotope exchange.

---
+++ F0AM-4.0.1/Chamber/Input_Files/O_Isotopes/HOx_O2_Exchange/Chamber_5_Iso_No_HOx_O2.m
@@ -0,0 +1,62 @@
+% Chamber_5_Iso.m
+% This is the input file for the 
+% O isotope simulation of Chamber
+% experiment 5.
+% 20231007 WW
+
+clear
+
+%% METEOROLOGY
+
+Met = {...
+%   names       values          
+    'P'         1013                       ; %Pressure, mbar
+    'T'         295                        ; %Temperature, K
+    'RH'        1                          ; %Relative Humidity, percent
+    'LFlux'     'GTLightFlux_2019.txt'     ; %Text file for radiation spectrum
+    'jcorr'     1                          ; %light attenuation factor
+    };
+
+%% CHEMICAL CONCENTRATIONS
+
+InitConc = {...
+%   names       conc(ppb)           HoldMe
+    'API'        25                 0;
+    'NO'         125                0;
+    'H2O2'       2000               0;
+};
+
+%% CHEMISTRY
+
+ChemFiles = {...
+    'RACM2_K(Met)';
+    'RACM2_J(Met,1)'; %Jmethod flag of 1 specifies using "BottomUp" J-value method.
+    'ICOIN_RACM2_No_HOx_O2';
+    };
+
+%% DILUTION CONCENTRATIONS
+% We are not diluting the chamber air, so this input is irrelevant (but still necessary).
+
+BkgdConc = {...
+%   names           values
+    'DEFAULT'       0;   %0 for all zeros, 1 to use InitConc
+    };
+
+%% OPTIONS
+
+ModelOptions.Verbose       = 1;
+ModelOptions.EndPointsOnly = 0;
+ModelOptions.LinkSteps     = 0;
+ModelOptions.IntTime       = 4*3600;
+ModelOptions.SavePath      = 'Output_Chamber_5_O_Isotope_No_HOx_O2.mat';
+ModelOptions.GoParallel    = 0;
+
+%% MODEL RUN
+% Now we call the model.
+% Output will be saved in the "SavePath" above and will also be written to the structure S.
+% Let's also throw away the inputs (don't worry, they are saved in the output structure).
+
+S = F0AM_ModelCore(Met,InitConc,ChemFiles,BkgdConc,ModelOptions);
+% clear Met InitConc ChemFiles BkgdConc ModelOptions
+
+
